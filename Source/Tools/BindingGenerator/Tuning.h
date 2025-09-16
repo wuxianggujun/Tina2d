a23764e0dc1f275fa57c14db710d32f924723700
wuxianggujun <3344207732@qq.com>
2025-09-16 15:15:42 +0800

    移除了3D的一些库和代码

--- Source/Tools/BindingGenerator/Tuning.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2008-2023 the Urho3D project
-// License: MIT
-
-#pragma once
-
-#include <string>
-
-std::string InsideDefine(const std::string& headerFile);
-bool IsIgnoredHeader(const std::string& headerFile);
